1806a7c962b90fd92b687fbbac963760a365f1c5
aviflombaum <avi@flombaum.com>
2019-05-22 11:18:06 -0400

    Added a comment of DB Connection

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,14 @@
 require 'bundler/setup'
 
 Bundler.require
+#             WHICH DATABASE               WHAT COMMAND
+# sqlite3 ./db/blog-development.sqlite < 'CREATE TABLE authors ()'
 
-DB = SQLite3::Database.new("./db/blog-development.sqlite")
+# sqlite3 ./db/blog-development.sqlite WHICH DATABASE
+DB = SQLite3::Database.new("./db/blog-development.sqlite") 
+
+# DB.execute("CREATE TABLE authors ()") # < 'CREATE TABLE authors ()' WHAT COMMAND
+
 
 require_all 'app'
 require_relative '../db/migrate/create_posts_table'
